d1e2087f8861bd3a1d19095e57a0764fc92702ff
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-10-02 23:19:21 -0300

    Initial Commit

---
+++ Aztec Complex/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Aztec Complex
+//
+//  Created by Andres Barbudo Rodriguez on 02-10-21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
